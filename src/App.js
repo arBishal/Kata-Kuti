9fc01c97e34c3a39ae12ca9fbf5956e226a8aa32
arahman <ashikur.rahman@dsinnovators.com>
2023-11-11 23:26:24 +0600

    Language changed

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ export default function App() {
   return (
     <div className="main">
       
-      <h1>Cit Cat Coe</h1>
+      <h1>কাটাকুটি</h1>
       
       <div className="board">
       
-        <button className="box">X</button>
-        <button className="box">X</button>
-        <button className="box">X</button>
+        <button className="box">১</button>
+        <button className="box">২</button>
+        <button className="box">৩</button>
 
-        <button className="box">X</button>
-        <button className="box">X</button>
-        <button className="box">X</button>
+        <button className="box">৪</button>
+        <button className="box">৫</button>
+        <button className="box">৬</button>
 
-        <button className="box">X</button>
-        <button className="box">X</button>
-        <button className="box">X</button>
+        <button className="box">৭</button>
+        <button className="box">৮</button>
+        <button className="box">৯</button>
       
       </div>
 
