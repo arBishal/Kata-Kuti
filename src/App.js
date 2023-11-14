af7470f6c614064923dcb5965345a52781ed769f
arahman <ashikur.rahman@dsinnovators.com>
2023-11-14 19:43:46 +0600

    Refactor code and make modular

--- src/App.js
+++ src/App.js
@@ -1,60 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-
-function Box({ value, onBoxClick }) {
-  return (
-    <button className="box" onClick={onBoxClick}>
-      {value}
-    </button>
-  );
-}
-
-function Board({xNext, board, onPlay}) {
-  const [count, setCount] = useState(0);
-  
-  function handleClick(index) {
-    setCount(count + 1);
-
-    if(board[index] || winnerCalculation(board)) return;
-
-    const nextBoard = board.slice();
-    nextBoard[index] = xNext ? "X" : "O";
-
-    onPlay(nextBoard);
-  }
-
-  const winner = winnerCalculation(board);
-
-  let status;
-  if (count === 0) status = "শুরু করবে: কাটা";
-  else {
-    if (winner) {
-      if (winner === "X") status = "কাটা জিতেছে!";
-      else if (winner === "O") status = "কুটি জিতেছে!";
-    } else if (winner === null && count > 8) status = "কেউ জিতেনি!";
-    else status = "এবার খেলবে: " + (xNext ? "কাটা" : "কুটি");
-  }
-
-  return (
-    <>
-      <div className="status">{status}</div>
-
-      <div className="board">
-        <Box value={board[0]} onBoxClick={() => handleClick(0)} />
-        <Box value={board[1]} onBoxClick={() => handleClick(1)} />
-        <Box value={board[2]} onBoxClick={() => handleClick(2)} />
-
-        <Box value={board[3]} onBoxClick={() => handleClick(3)} />
-        <Box value={board[4]} onBoxClick={() => handleClick(4)} />
-        <Box value={board[5]} onBoxClick={() => handleClick(5)} />
-
-        <Box value={board[6]} onBoxClick={() => handleClick(6)} />
-        <Box value={board[7]} onBoxClick={() => handleClick(7)} />
-        <Box value={board[8]} onBoxClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
+import Board from "./Board";
 
 export default function App() {
   const [historyBoard, setHistoryBoard] = useState([Array(9).fill(null)]);
@@ -63,7 +9,10 @@ export default function App() {
   const xNext = currentMove % 2 === 0;
 
   function handlePlay(nextBoard) {
-    const nextHistoryBoard = [...historyBoard.slice(0, currentMove+1), nextBoard];
+    const nextHistoryBoard = [
+      ...historyBoard.slice(0, currentMove + 1),
+      nextBoard,
+    ];
     setHistoryBoard(nextHistoryBoard);
     setCurrentMove(nextHistoryBoard.length - 1);
   }
@@ -77,7 +26,9 @@ export default function App() {
     if (move > 0) description = "চাল #" + move;
     else description = "শুরু!";
     return (
-        <button key={move} onClick={() => jumpTo(move)}>{description}</button>
+      <button key={move} onClick={() => jumpTo(move)}>
+        {description}
+      </button>
     );
   });
 
@@ -87,29 +38,7 @@ export default function App() {
 
       <Board xNext={xNext} board={currentBoard} onPlay={handlePlay} />
 
-      <div className="history">
-        {moves}
-      </div>
+      <div className="history">{moves}</div>
     </div>
   );
-}
-
-function winnerCalculation(board) {
-  const moves = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < moves.length; i++) {
-    const [a, b, c] = moves[i];
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a];
-    }
-  }
-  return null;
 }
---
+++ src/Board.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import winnerCalculation from "./winnerCalculation";
+
+function Box({ value, onBoxClick }) {
+  return (
+    <button className="box" onClick={onBoxClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board({ xNext, board, onPlay }) {
+  const [count, setCount] = useState(0);
+
+  function handleClick(index) {
+    setCount(count + 1);
+
+    if (board[index] || winnerCalculation(board)) return;
+
+    const nextBoard = board.slice();
+    nextBoard[index] = xNext ? "X" : "O";
+
+    onPlay(nextBoard);
+  }
+
+  const winner = winnerCalculation(board);
+
+  let status;
+  if (count === 0) status = "শুরু করবে: কাটা";
+  else {
+    if (winner) {
+      if (winner === "X") status = "কাটা জিতেছে!";
+      else if (winner === "O") status = "কুটি জিতেছে!";
+    } else if (winner === null && count > 8) status = "কেউ জিতেনি!";
+    else status = "এবার খেলবে: " + (xNext ? "কাটা" : "কুটি");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+
+      <div className="board">
+        <Box value={board[0]} onBoxClick={() => handleClick(0)} />
+        <Box value={board[1]} onBoxClick={() => handleClick(1)} />
+        <Box value={board[2]} onBoxClick={() => handleClick(2)} />
+
+        <Box value={board[3]} onBoxClick={() => handleClick(3)} />
+        <Box value={board[4]} onBoxClick={() => handleClick(4)} />
+        <Box value={board[5]} onBoxClick={() => handleClick(5)} />
+
+        <Box value={board[6]} onBoxClick={() => handleClick(6)} />
+        <Box value={board[7]} onBoxClick={() => handleClick(7)} />
+        <Box value={board[8]} onBoxClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
